28182afeb64cca8808b8e32bd3c0d64022d73d6b
WPH <peiheweng@gmail.com>
2025-01-11 10:55:17 +0800

    feat(frontend): add skeleton for react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Landing from './Pages/Landing';
+import Requests from './Pages/Requests';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Test ANT Design button</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+       <Route path="/" element={<Login />} />
+       <Route path="/landing" element={<Landing/>} />
+       <Route path="/requests" element={<Requests />} />
+    </Routes>
+ </>
   );
 }
 
