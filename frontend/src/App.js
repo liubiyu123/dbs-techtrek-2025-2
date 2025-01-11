2008e172038353f858f6d8d061bf61956b79c2c5
Brendon Tan <brendontan003@gmail.com>
2025-01-11 15:50:43 +0800

    biddingPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,24 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Pages/Login";
 import Landing from "./Pages/Landing";
 import Requests from "./Pages/Requests";
+import Bidding from "./Pages/Bidding";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('token');
-  
+  const isAuthenticated = localStorage.getItem("token");
+
   if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
+    return <Navigate to='/' replace />;
   }
-  
+
   return children;
 };
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path='/' element={<Login />} />
       <Route
-        path="/landing"
+        path='/landing'
         element={
           <ProtectedRoute>
             <Landing />
@@ -27,14 +28,22 @@ function App() {
         }
       />
       <Route
-        path="/requests"
+        path='/requests'
         element={
           <ProtectedRoute>
             <Requests />
           </ProtectedRoute>
         }
       />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route
+        path='/bidding'
+        element={
+          <ProtectedRoute>
+            <Bidding />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
   );
 }
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,50 +5,56 @@ import { Button, Layout, Grid, Menu, Space, theme } from "antd";
 const { Header, Content, Footer } = Layout;
 
 const NavBar = () => {
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        navigate('/'); 
-        };
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
-    return (
-        <nav>
-            <Header style={{
-                position: 'sticky',
-                top: 0,
-                zIndex: 1,
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                }}> 
-                <div>
-                    <Button type = 'primary' style={{
-                    marginRight: '16px'
-                }}>
-                        <NavLink to="/Landing">Landing Page</NavLink>
-                    </Button>
+  return (
+    <nav>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Button
+            type='primary'
+            style={{
+              marginRight: "16px",
+            }}
+          >
+            <NavLink to='/Landing'>Landing Page</NavLink>
+          </Button>
 
-                    <Button type = 'primary' style={{
-                }}>                
-                        <NavLink to="/Requests">Received Requests</NavLink>
-                    </Button>
-                </div>
-                <div>
-                <Button danger type = 'primary' onClick={handleLogout}>
-                    <NavLink to="/">Logout</NavLink>
-                </Button>
-                </div>
-                
-            </Header>
-        </nav>
+          <Button type='primary' style={{}}>
+            <NavLink to='/Requests'>Received Requests</NavLink>
+          </Button>
+
+          <Button
+            type='primary'
+            style={{
+              marginLeft: "16px",
+            }}
+          >
+            <NavLink to='/Bidding'>Bidding Page</NavLink>
+          </Button>
+        </div>
+        <div>
+          <Button danger type='primary' onClick={handleLogout}>
+            <NavLink to='/'>Logout</NavLink>
+          </Button>
+        </div>
+      </Header>
+    </nav>
   );
 };
 
 export default NavBar;
-
-
-
-
-
-
