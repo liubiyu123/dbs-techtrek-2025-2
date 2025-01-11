f6acc3e4dab3d4f7c47c948cd5d62586612abb63
iujinwee <eugeneftw500@gmail.com>
2025-01-11 11:46:20 +0800

    Added skeleton for models

--- backend/src/controllers/requests.js
+++ backend/src/controllers/requests.js
@@ -1,5 +1,46 @@
 class RequestController {
+  /***
+   *  Create new company requests into outstandingRequests & requestReceived tables
+   */
   public createRequest = () => {
+    try{
+      console.log("TEST")
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  /***
+   *  Get all requests made by the company
+   */
+  public getAllCompanyRequests = () => {
+    try{
+      console.log("TEST")
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  /***
+   *  Get outstanding requests
+   */
+  public getCompanyRequests = () => {
+    try{
+      console.log("TEST")
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  public editCompanyRequest = () => {
+    try{
+      console.log("TEST")
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  public deleteCompanyRequest = () => {
     try{
       console.log("TEST")
     }catch (e) {
