b8f506375dcf7d5572a44461c35cbc13b8c52a75
iujinwee <eugeneftw500@gmail.com>
2025-01-11 15:41:48 +0800

    Updated AccountController functionalities

--- backend-new/src/controllers/accounts.ts
+++ backend-new/src/controllers/accounts.ts
@@ -1,12 +1,16 @@
 import OutstandingRequest from '../models/requests';
 
 import {Request, Response} from "express";
+import AccountModel from "@models/account";
 
 class AccountController {
   //  GET /:id (get account details)
   public getAccountDetails = async (req: Request, res: Response) => {
     try {
-      res.status(201).json({message:"test"})
+      // Return the account information
+      const {id} = req.params
+      const account = await AccountModel.findOne({id})
+      res.status(201).json({data: account})
       
     } catch (error) {
       res.status(500).json({ message: 'Failed to get account details', error: error});
@@ -17,13 +21,13 @@ class AccountController {
   public updateAccountDetails = async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
-      const {cash } = req.body
+      const newData = req.body
 
-      const requests = await OutstandingRequest.updateOne({ id, cash: cash });
-      res.status(200).json({ data: requests });
+      const updatedAccount = await AccountModel.findOneAndUpdate({ id}, newData);
+      res.status(200).json({ data: updatedAccount });
 
     } catch (error) {
-      res.status(500).json({ message: 'Failed to fetch requests', error: error });
+      res.status(500).json({ message: 'Failed to update account details', error: error });
     }
   }
 }
